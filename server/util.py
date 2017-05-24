7123ea5d07ae462c03630b3b97e8bec388e0a374
fabio ellena <ellena.fabio@gmail.com>
2017-05-24 10:49:43 +0200

    interlinking notebook, poi enrichment, minor server changes

--- server/util.py
+++ server/util.py
@@ -43,7 +43,8 @@ def create_playlist_name(pois):
     ids = [str(x['id']) for x in pois]
     weights = [str(x['weight']) for x in pois]
     lists = zip(ids, weights)
-    lists = sorted(lists,key= lambda x: x[1],reverse=True)
+    lists = sorted(lists,key= lambda x: (x[1], x[2]),reverse=True)
+    
     groups = [':'.join(x) for x in lists]
     name = '_'.join(groups)
     return name
